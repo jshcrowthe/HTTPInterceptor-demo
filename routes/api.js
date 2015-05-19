58a31c47c960e5a9ab1b192ba65fc2c097b1f7e1
Josh Crowther <jshcrowthe@gmail.com>
2015-05-18 23:14:24 -0600

    Error style change

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ var counter = 0;
 router.get('/authRequired', function(req, res, next) {
   if (req.headers.authorization === 'Bearer my-super-secret-auth-token') return res.send(faker.helpers.createCard());
   return res.status(401).send({
-    message: 'ERROR: Unauthorized'
+    message: 'Unauthorized'
   });
 });
 
@@ -19,7 +19,7 @@ router.get('/unreliable', function(req, res, next) {
   if (counter % 6 === 0) return res.send(faker.helpers.createCard());
   setTimeout(function() {
     return res.status(503).send({
-      message: 'ERROR: API is unreliable and didn\'t return this time around'
+      message: 'API is unreliable and didn\'t return this time around'
     });
   }, 250);
 });
